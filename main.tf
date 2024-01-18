044fafb9f37cfdad0829b5c09131e7630d9075d1
Ivan Junior <ivan_junior_we@hotmail.com>
2024-01-18 17:08:15 -0300

    Criação de uma EC2 e Key Pair para acesso à EC2.

---
+++ main.tf
@@ -0,0 +1,45 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+  #access_key = var.access_key
+}
+
+
+###################### Key Pair ######################
+resource "aws_key_pair" "ssh_key" {
+  key_name   = "ssh_key"
+  public_key = var.access_key
+}
+###################### ### #### ######################
+
+
+###################### EC2 ######################
+resource "aws_instance" "ec2" {
+  ami           = "ami-01c146ce0993b7884" #Ubuntu-2023-10-27T15-45 # aws ec2 describe-images --owners amazon --query 'Images[*].[ImageId, Name]' --output table --region us-east-1 | grep Ubuntu-2023-10
+  instance_type = "t3.small"              # aws ec2 describe-instance-types --query 'InstanceTypes[*].[InstanceType]' --output table | grep small
+
+  tags = {
+    Name = "EC2"
+  }
+
+  key_name = aws_key_pair.ssh_key.key_name
+}
+###################### ### ######################
+
+
+##################### Variables #####################
+variable "access_key" {
+}
+##################### ######### #####################
+
+
+###################### Outputs ######################
+###################### ####### ######################
