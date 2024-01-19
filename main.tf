44b40ced415b8e4535e9bec7d63c8aa14a684d1b
Ivan Junior <ivan_junior_we@hotmail.com>
2024-01-19 12:17:42 -0300

    Atualização do comentário que contem o comando que mostra os tipos de máquinas e suas especificações.

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ resource "aws_key_pair" "ssh_key" {
 ###################### EC2 ######################
 resource "aws_instance" "ec2" {
   ami           = "ami-01c146ce0993b7884" #Ubuntu-2023-10-27T15-45 # aws ec2 describe-images --owners amazon --query 'Images[*].[ImageId, Name]' --output table --region us-east-1 | grep Ubuntu-2023-10
-  instance_type = "t3.small"              # aws ec2 describe-instance-types --query 'InstanceTypes[*].[InstanceType]' --output table | grep small
+  instance_type = "t3.small"              # aws ec2 describe-instance-types --query 'InstanceTypes[*].[InstanceType, VCpuInfo.DefaultVCpus, MemoryInfo.SizeInMiB, NetworkInfo.NetworkPerformance, StorageInfo.TotalVolumeSize]' --output table | grep small
 
   tags = {
     Name = "EC2"
