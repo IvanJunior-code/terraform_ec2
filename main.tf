2d1523d51cebf904a216cb283e677a145a869242
Ivan Junior <ivan_junior_we@hotmail.com>
2024-01-25 12:03:18 -0300

    Ajuste no nome dos recursos.

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
 
 
 ###################### Key Pair ######################
-resource "aws_key_pair" "ssh_key" {
+resource "aws_key_pair" "ssh_key_terraform" {
   key_name   = "ssh_key"
   public_key = var.access_key
 
@@ -22,7 +22,7 @@ resource "aws_key_pair" "ssh_key" {
 
 
 ###################### EC2 ######################
-resource "aws_instance" "ec2" {
+resource "aws_instance" "ec2_terraform" {
   ami                    = "ami-01c146ce0993b7884" #Ubuntu-2023-10-27T15-45 # aws ec2 describe-images --owners amazon --query 'Images[*].[ImageId, Name]' --output table --region us-east-1 | grep Ubuntu-2023-10
   instance_type          = "t3.small"              # aws ec2 describe-instance-types --query 'InstanceTypes[*].[InstanceType, VCpuInfo.DefaultVCpus, MemoryInfo.SizeInMiB, NetworkInfo.NetworkPerformance, StorageInfo.TotalVolumeSize]' --output table | grep small
   subnet_id              = aws_subnet.subnet_terraform.id
@@ -33,7 +33,7 @@ resource "aws_instance" "ec2" {
     ManagedBy = var.tags_ManagedBy
   }
 
-  key_name = aws_key_pair.ssh_key.key_name
+  key_name = aws_key_pair.ssh_key_terraform.key_name
 }
 ###################### ### ######################
 
