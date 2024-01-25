899e6cbaf308fef2beabb031c5b90a0ad993add0
Ivan Junior <ivan_junior_we@hotmail.com>
2024-01-25 11:05:52 -0300

    Adicionando comando de conexão ssh com o IP da EC2.

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,8 @@
 output "public_ip_ec2" {
   value = aws_instance.ec2.public_ip
 }
+
+output "connect_ssh" {
+  value = "ssh -i ~/.ssh/key ubuntu@${aws_instance.ec2.public_ip}"
+}
 ###################### ####### ######################
