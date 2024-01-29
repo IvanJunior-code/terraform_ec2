6da3c7dad4aa36ea2a089a03b268fcf6a65f5f34
Ivan Junior <ivan_junior_we@hotmail.com>
2024-01-28 22:13:27 -0300

    Removendo o output do IP público da EC2.

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 ###################### Outputs ######################
-output "public_ip_ec2" {
-  value = aws_instance.ec2_terraform.public_ip
-}
+# output "public_ip_ec2" {
+#   value = aws_instance.ec2_terraform.public_ip
+# }
 
 output "connect_ssh" {
-  value = "ssh ubuntu@${aws_instance.ec2_terraform.public_ip} -i ~/.ssh/key"
+  value = "ssh ubuntu@${aws_eip.eip_terraform.public_ip} -i ~/.ssh/key"
 }
 ###################### ####### ######################
