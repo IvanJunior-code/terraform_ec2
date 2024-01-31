74a9c2638cec11e03f1b6447e47179c8d75a675d
Ivan Junior <ivan_junior_we@hotmail.com>
2024-01-31 17:59:54 -0300

    Adicionado backend para armazenar o terraform.tfstate no bucket S3.

--- main.tf
+++ main.tf
@@ -1,4 +1,9 @@
 terraform {
+  backend "s3" {
+    bucket = "s3-bucket-terraform-ivan"
+    key    = "terraform-state/terraform.tfstate"
+    region = "us-east-1"
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
